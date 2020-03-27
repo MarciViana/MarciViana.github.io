3a422ea26cab9199f7963b9c063ce718eccdd95f
MarciViana <marcelavp@terra.com.br>
2020-03-27 12:32:11 +0000

    alt caminho solucoes.html

--- build_pdf.js
+++ build_pdf.js
@@ -615,7 +615,7 @@ function codigosBase(){
 function geraSolucoes()
 {
 
-  window.open("solucoes.html", 'solucoes');    
+  window.open("https://marciviana.github.io/solucoes.html", 'solucoes');    
     
 }
 
